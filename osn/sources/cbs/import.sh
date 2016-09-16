84974f46395c0581399b5780d0a3d80a84285f19
Lex Slaghuis <lex@wikiwise.nl>
2016-09-16 15:23:15 +0200

    Renaming cbs files

--- osn/sources/cbs/import.sh
+++ osn/sources/cbs/import.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-sudo docker run -it --rm --name my-running-script -v "$PWD":/usr/src/myapp -w /usr/src/myapp php:7.0-cli php source2csv.php
+sudo docker run -it --rm --name my-running-script -v "$PWD/../..":/usr/src/myapp -w /usr/src/myapp php:7.0-cli php sources/cbs/cbs2csv.php
+
+
+#sudo docker run -it --rm --name my-running-script -v "$PWD/..":/usr/src/myapp -w /usr/src/myapp php:7.0-cli php tests/test.php
